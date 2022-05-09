390f9d09db9a42b37eb2ca659fc849da0b8089d2
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-05-09 18:06:11 +0900

    Create Day_15.swift

---
+++ Basic/Day_15/Day_15.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// final class
+
+// final keyword does not Inherit Other Classes
+final class Friend {
+    var name : String
+    
+    init(name : String) {
+        self.name = name
+    }
+}
+
+/*// Inherited as : Friend, but the Friend class is a final class, so a Compilation error occurs
+class BestFriend : Friend {
+    
+    override init(name : String) {
+        super.init(name: "Best Friend" + name)
+    }
+}*/
+
+let myFriend = Friend(name: "Paul")
+print("myFriend : \(myFriend.name)")
+
+/*let myBestFriend = Friend(name: "Anthony")
+print("myBestFriend : \(myBestFriend.name)")*/
