ffefdd6840d31c60d2a7af2ebe31034a636d88ea
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-05-09 18:04:13 +0900

    Create Day_14.swift

---
+++ Basic/Day_14/Day_14.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+// Dictionary - consist of key and value
+// 구성 - key : value
+// A pair of dictionaries with key values - similar to lockers
+var myFriend = ["bestfriend" : "Paul", "highSchool" : "Daivd"]
+
+/*let myBestFriend = myFriend["bestfriend"]
+let highSchoolFriend = myFriend["highSchool"]
+let youtubeFriend = myFriend["youtube", default: "No Friend"]
+
+// Change the value
+myFriend["bestfriend"] = "Anthoy"
+
+let myBF = myFriend["bestfriend"]
+
+// input the new data
+myFriend["newFriend"] = "Tom"
+let newFriend = myFriend["newFriend"]
+
+myFriend.updateValue("Luna", forKey: "girlFriend")
+let girlFriend = myFriend["girlFriend"]
+
+myFriend.updateValue("Jack", forKey: "bestfriend")
+let myBF2 = myFriend["bestfriend"]*/
+
+
+
+if let myBestFriend = myFriend["bestfriend"] {
+    print(myBestFriend)
+}
+
+if let highSchoolFriend = myFriend["highSchool"] {
+    print(highSchoolFriend)
+}
+
+// Set to get default value if no value is saved with the key you look for when pulling out
+let youtubeFriend = myFriend["youtube", default: "No Friend"]
+print(youtubeFriend)
+
+// 값을 추가 및 변경 가능
+myFriend["bestfriend"] = "Anthoy"
+let myBF = myFriend["Anthoy", default: "No Friend"]
+print(myBF)
+
+myFriend["newFriend"] = "Tom"
+let newFriend = myFriend["newFriend", default: "No Friend"]
+
+myFriend.updateValue("Luna", forKey: "girlFriend")
+let girlFriend = myFriend["girlFriend", default: "girlFriend"]
+print(girlFriend)
+
+myFriend.updateValue("Jack", forKey: "bestfriend")
+let myBF2 = myFriend["bestfriend", default: "girlFriend"]
+print(myBF2)
+
+// emptyDictonary
+let emptyDictionary : [String : Int] = [:]
+let emptyDictionary2 = [String : Int]()
+let emptyDictionary3 : [String : Int] = Dictionary<String, Int>()
+
+myFriend.count
+
+for item in myFriend {
+    print("item : ", item)
+}
