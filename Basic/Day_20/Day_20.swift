dd5cce09546af89e2d2f41233a49aaa0fdde71f4
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-05-18 17:56:16 +0900

    Create Day_20.swift

---
+++ Basic/Day_20/Day_20.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Struct method
+// struct can have methods and can use member variables
+struct Friend {
+    var name : String
+    var age : Int
+    
+    func sayHi() -> String {
+        print("sayHi")
+        return "Hello, I am \(name), and I am \(age)"
+    }
+}
+
+var myFriend = Friend(name: "Paul", age: 25)
+print(myFriend.sayHi())
